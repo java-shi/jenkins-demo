fdcfae4e489af7a698dddbef2420fe6efb6ede0a
java-shi <631288464@qq.com>
2020-07-01 17:22:08 +0800

    test

--- src/main/java/com/sfy/controller/DemoController.java
+++ src/main/java/com/sfy/controller/DemoController.java
@@ -14,6 +14,6 @@ public class DemoController {
 
     @RequestMapping(method = RequestMethod.GET,path = "/getDemo")
     public String getDemo(){
-        return "demo1";
+        return "demo12";
     }
 }
